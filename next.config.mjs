c429d756faff0a8376c23399d3995d137195e856
Luiz P Ragi <128090243+lpragi09@users.noreply.github.com>
2026-01-03 03:35:26 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Permite que o build termine mesmo se houver erros de TypeScript
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Impede que avisos de variáveis não usadas travem o deploy
     ignoreDuringBuilds: true,
   },
 };
-
 export default nextConfig;
